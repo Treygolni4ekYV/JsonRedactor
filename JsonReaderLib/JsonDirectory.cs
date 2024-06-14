6aa72eb1181b3f1fbbb4f59dd42f956f84f6452d
Treygolni4ekYV <ilya.klachkov@yandex.by>
2024-06-14 22:27:21 +0300

    JsonDir class addet

---
+++ JsonReaderLib/JsonDirectory.cs
@@ -0,0 +1,33 @@
+﻿
+namespace JsonReaderLib
+{
+    public class JsonDirectory
+    {
+        public readonly string FullDirPath;
+        private List<string> FilesPaths = new();
+
+        public JsonDirectory(string dirPath)
+        {
+            FullDirPath = Path.GetFullPath(dirPath);
+            FilesPaths.AddRange(
+                ExtractJsonPatches(
+                    Directory.GetFileSystemEntries(FullDirPath).ToList<string>())
+                );
+        }
+
+        //Достаем только json aайлы
+        private List<string> ExtractJsonPatches(List<string> filePatches)
+        {
+            List<string> jsonPatches = new();
+            foreach (var patch in filePatches)
+            {
+                if (Path.GetExtension(patch) == ".json")
+                {
+                    jsonPatches.Add(patch);
+                }
+            }
+            return jsonPatches;
+        }
+
+    }
+}
